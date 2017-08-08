2563edbe04ef5a9c7d95094f543e4d28e123721a
mariia_suvalova <mariia_suvalova@epam.com>
2017-08-08 21:23:42 +0300

    MonkeyCell

---
+++ VTSClient.Core/VTSClient.Core/Test/MonkeysViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MvvmCross.Core.ViewModels;
+
+namespace VTSClient.Core.Test
+{
+	public class MonkeysViewModel : MvxViewModel
+	{
+		public MvxObservableCollection<Monkey> Monkeys => new MvxObservableCollection<Monkey>();
+
+		public MonkeysViewModel()
+		{
+			Monkeys.Add(new Monkey
+			{
+				Name = "Baboon",
+				Location = "Africa & Asia",
+				Details = "Baboons are African and Arabian Old World monkeys belonging to the genus Papio, part of the subfamily Cercopithecinae.",
+				Image = "http://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Papio_anubis_%28Serengeti%2C_2009%29.jpg/200px-Papio_anubis_%28Serengeti%2C_2009%29.jpg"
+			});
+
+			Monkeys.Add(new Monkey
+			{
+				Name = "Capuchin Monkey",
+				Location = "Central & South America",
+				Details = "The capuchin monkeys are New World monkeys of the subfamily Cebinae. Prior to 2011, the subfamily contained only a single genus, Cebus.",
+				Image = "http://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Capuchin_Costa_Rica.jpg/200px-Capuchin_Costa_Rica.jpg"
+			});
+		}
+	}
+}
